0f627597a71897a282a23004fc38e37ebe4a26a8
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-09-29 17:36:55 -0500

    .

--- wm/wm.cc
+++ wm/wm.cc
@@ -14,8 +14,10 @@ WM& WM::start()
 
     scr = xcb_setup_roots_iterator(xcb_get_setup(dpy)).data;
 
-    uint32_t values = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT
-                      | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY;
+    uint32_t values = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
+            XCB_EVENT_MASK_STRUCTURE_NOTIFY |
+            XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY |
+            XCB_EVENT_MASK_PROPERTY_CHANGE;
     xcb_change_window_attributes_checked(dpy, scr->root, XCB_CW_EVENT_MASK, &values);
     xcb_flush(dpy);  // TODO
 
