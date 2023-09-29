b67eacab92212695407461dadde570cdeb952254
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-09-29 14:53:04 -0500

    .

--- wm/wm.cc
+++ wm/wm.cc
@@ -11,6 +11,12 @@ WM& WM::start()
 
     scr = xcb_setup_roots_iterator(xcb_get_setup(dpy)).data;
 
+    uint32_t values = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT
+                      | XCB_EVENT_MASK_STRUCTURE_NOTIFY
+                      | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY
+                      | XCB_EVENT_MASK_PROPERTY_CHANGE;
+    xcb_change_window_attributes_checked(dpy, scr->root, XCB_CW_EVENT_MASK, &values);
+
     xcb_grab_button(dpy, 0, scr->root, XCB_EVENT_MASK_BUTTON_PRESS |
                                         XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
                     XCB_GRAB_MODE_ASYNC, scr->root, XCB_NONE, 1, XCB_MOD_MASK_ANY);
