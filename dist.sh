c50517628f53db3c12f13c9e9dd20e83ae30e459
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-11-22 09:22:40 -0600

    .

--- dist.sh
+++ dist.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
 
+VERSION=$(<PRJ_VERSION)
+
+make -C contrib/luaw distclean
+mkdir customwm-$VERSION
+rsync -lrv --exclude=.git --exclude=.old --exclude=customwm-$VERSION --exclude=*.o --exclude=.gitignore --exclude=.gitmodules --exclude=.idea --exclude=*.a --exclude=*.so --exclude=luazh-jit.dSYM . customwm-$VERSION
+tar -cvjSf customwm-$VERSION.tar.bz2 customwm-$VERSION
+rm -rf customwm-$VERSION
