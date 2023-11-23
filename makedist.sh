02b0ca62d077c74cda2af3b6e316c90a1d5a199d
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-11-23 13:42:06 -0600

    .

--- makedist.sh
+++ makedist.sh
@@ -4,6 +4,7 @@ VERSION=$(<PRJ_VERSION)
 
 make -C contrib/luaw distclean
 git clean -fdx
+git submodule update --init --remote --recursive
 mkdir customwm-$VERSION
 rsync -lrv --exclude=.git --exclude=.old --exclude=customwm-$VERSION --exclude=*.o --exclude=.gitignore --exclude=.gitmodules --exclude=.idea --exclude=*.a --exclude=*.so --exclude=luazh-jit.dSYM --exclude=.github . customwm-$VERSION
 tar -cvjSf customwm-$VERSION.tar.bz2 customwm-$VERSION
